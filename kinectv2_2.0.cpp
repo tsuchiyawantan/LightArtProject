c7665855c02af0c84c2b08478c00776f0ba794bf
Momoko Tsuchiya <tsuchiya.momoko@is.ocha.ac.jp>
2016-11-24 17:06:21 +0900

    made new branch

--- kinectv2_2.0.cpp
+++ kinectv2_2.0.cpp
@@ -8,35 +8,26 @@
 #include "Log.h"
 #include "CatmullSpline.h"
 #include "NtKinect.h"
-#define HUE 180
+#define HUE 10
 #define SPACESIZE 10
 #define SCALESIZE 1
 #define FILTERSIZE 81
 
-string hstate[] = { "unknown", "nottracked", "Open", "Closed", "Lasso" };
-string hconf[] = { "low", "high" };
+Dot dot;
+CatmullSpline catmull;
+vector<cv::Mat> after_img;
 
-string str(pair<int, int> p) {
-	stringstream ss;
-	ss << hstate[p.first] << ":" << hconf[p.second];
-	return ss.str();
-}
-
-string str(pair<int, int> left, pair<int, int>right) {
-	stringstream ss;
-	ss << str(left) << " " << str(right);
-	return ss.str();
-}
 void doCatmull(cv::Mat &srcImg, cv::Mat &resultImg, vector<vector<pair<int, int>>> &approximationLine){
-	CatmullSpline catmull;
+	catmull.init();
 	for (int i = 0; i < approximationLine.size(); i++){
 		catmull.drawLine(resultImg, approximationLine[i], HUE);
 	}
 	cv::blur(resultImg, resultImg, cv::Size(9, 9));
 	catmull.drawInline(resultImg, HUE);
+	after_img.push_back(resultImg);
 }
 void doDot(cv::Mat &srcImg, cv::Mat &resultImg){
-	Dot dot;
+	dot.init();
 	dot.setWhiteDots(srcImg);
 	dot.findStart(srcImg);
 	dot.makeLine(srcImg);
@@ -50,7 +41,8 @@ void main() {
 		Depth depth;
 		Log log;
 		log.Initialize("logPOINTER.txt");
-		//int count = 0;
+		int count = 0;	
+		//cv::Mat result_img = cv::Mat(100, 100, CV_8UC3, cv::Scalar(0, 0, 0));
 		while (1) {
 			clock_t start = clock();
 			depth.setBodyDepth();
@@ -60,14 +52,16 @@ void main() {
 			//cv::imshow("normalize depth image", depth.normalizeDepthImage);
 			depth.setContour(depth.normalizeDepthImage);
 			//cv::imshow("contour image", depth.contourImage);
-			cv::Mat resultImg = cv::Mat(depth.contourImage.rows, depth.contourImage.cols, CV_8UC3, cv::Scalar(0, 0, 0));
+			cv::Mat result_img = cv::Mat(depth.contourImage.rows, depth.contourImage.cols, CV_8UC3, cv::Scalar(0, 0, 0));
 
-			doDot(depth.contourImage, resultImg);
+			doDot(depth.contourImage, result_img);
 			//cv::imshow("complete image", depth.contourImage);
-		//cv:imwrite("image/img" + to_string(count) + ".png", resultImg);
-			cv::imshow("Catmull Spline", resultImg);
-		
-			//count++;
+			//cv:imwrite("image/img" + to_string(count) + ".png", resultImg);
+			cv::imshow("Catmull Spline", result_img);
+			//doEffect();
+
+
+			count++;
 			auto key = cv::waitKey(20);
 			if (key == 'q') break;
 		}
